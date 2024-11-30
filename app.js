7013180eb8f8fed74176ec4cfeb540a9e78acf29
♡°𝓕𝓪𝓽𝓲𝓶𝓪°杀叛徒 <iamqasim221@gmail.com>
2024-11-30 20:47:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,283 @@
+//TASK :01 
+// var h1= 'counting'
+// document.write(h1,'<br>','<br>')
+// for (var i=1; i<16; i++){
+//     document.write(  +i+" ,")
+// }
+
+// var h2= 'Reverse counting'
+// document.write( '<br>','<br>',h2,'<br>','<br>')
+// for (var j=10; j>=1; j--){
+//     document.write(j+" ,")
+// }
+
+// var h3= 'Even'
+// document.write( '<br>','<br>',h3,'<br>','<br>')
+// for (var k=0; k<=20; k=k+2){
+//     document.write(k+" ,")
+// }
+
+// var h4= 'odd'
+// document.write( '<br>','<br>',h4,'<br>','<br>')
+// for (var l=1; l<=20; l=l+2){
+    //     document.write(l+" ,")    
+    // }
+    
+    // var h5= 'series'
+    // document.write( '<br>','<br>',h5,'<br>','<br>')
+    // for (var m=0; m<=20; m=m+2){
+    //     document.write(m+"k"+" ,")    
+    // }
+    
+    
+    
+
+    
+    //task:02
+    // var arry =['Avengers','Spectre','jurassic world','inside out']
+    // for (var i=0; i<arry.length; i++){
+    //     document.write( "-)"+arry[i]+"<br> ")
+        
+    // }
+    // document.write('index of arry ='+ arry.length)
+    
+ 
+
+
+
+    
+    
+    
+    //TASK :03
+    
+    // var studentNames = ["Alice", "Bob", "Charlie"]
+    // var  studentScores = [450, 380, 400]
+    // for (let i = 0; i < studentNames.length; i++) {
+    //     var name = studentNames[i]
+    //     var score = studentScores[i]
+    //     var percentage = (score / 500) * 100 } 
+    
+    //     document.write(` name: ${name},<br>` )
+    //     document.write(`Score: ${score} / 500, <br>`)
+    //     document.write(`Percentage: ${percentage.toFixed(2)}% `)
+    
+    
+
+
+
+
+    
+    //task:04
+    // var PASSWORD='fatima'
+    // var inp=prompt('enter pasword')
+    // if (inp==PASSWORD){
+    //  alert("you've entered the right one password")
+    // }
+    // else if ( inp==''){
+    //  alert('please enter your password')
+    // }
+    // else {
+    //  alert('written pasword is wrong! Try again')
+    // }
+    
+    
+    
+    
+    
+    //task :05
+    // var userinput=+prompt('enter table name:')
+    // var inp=+prompt('enter length of table')
+    // var i;
+    //  for(i=1; i<=inp; i++){
+    //     document.write( userinput+'x' +i+'='+ userinput*i+ "<br>"  )  
+    //  } 
+
+
+
+
+
+//TASK:06    
+    
+// var store=prompt('welcome to ABC bakery.What you want to order sir/maam')    
+// var inp=['apple','apple pie','cookie','chips','patties']
+// var match=false
+//  for (var i=0; i<=inp.length; i++){
+                
+//      if (inp[i]==store){
+//         match=true          
+//          document.write('item found')
+//      }
+//  }
+// if(match==false){
+//     document.write('item not found')          
+// }
+
+
+
+
+
+
+//TASK:07
+
+    // var inp=(1*3*5*7)
+    // document.write( 'The product of odd integers from 1-7 is ='+inp)
+
+    
+    
+
+
+    
+    //TASK:08
+    // var arry=[10,20,3,40,10]
+    // var smallestno=arry[0]
+    // for (var i=1; i<arry.length; i++){
+    //   if (arry[i]<smallestno){
+    //     smallestno=arry[i]
+    //   }
+    // }
+    //     document.write(arry,"<br>")
+    //     document.write("smallest number",smallestno)
+    
+
+
+
+
+
+    //TASK NO:08
+    // var arry=[10,20,3,40,10]
+    // var largestno= arry[0]
+    //  for (var i=1; i<arry.length; i++)
+    //  {
+    //   if(arry[i]>largestno){
+    //     largestno=arry[i]
+    //   }
+    
+    //  }
+    
+    //      document.write(arry,"<br>")
+    //          document.write("largest n:",largestno)
+    
+
+
+
+    //task :09
+    //  var arry=[10,20,3,40,10]
+    //              var smallestno=arry[0]
+    //              for (var i=1; i<arry.length; i++){
+    //                if (arry[i]<smallestno){
+    //                  smallestno=arry[i]
+    //                }
+    //              }
+    //                  document.write(arry,"<br>")
+    //                  document.write("smallest number:",smallestno,'<br>')
+                 
+    
+    //                  var largestno= arry[0]
+    //                  for (var i=1; i<arry.length; i++)
+    //                  {
+    //                   if(arry[i]>largestno){
+    //                     largestno=arry[i]
+    //                   }
+    //                  }
+    //                      document.write("<br>")
+    //                          document.write("largest number:",largestno)
+
+
+
+
+
+
+    //TASK :10
+    // var inp=prompt('enter no')
+                        // if (inp%2===0){
+                        //     alert('no is even')
+                        // }
+                        // else{
+                        //     alert('odd')
+                        // }
+
+
+
+                        
+//TASK:11
+
+                                   
+ // var inp=prompt('enetr your name')
+            // if (inp=='fatima'){
+            //     document.write('congrats ')
+            // }
+            
+
+
+  
+
+
+            //TASK:12
+
+// var promptt = prompt('enter any no ')
+
+// if (promptt%3==0){
+//   alert('no is divisible by 3')
+// }
+
+// else {
+//   alert('no is not divisible by 3')
+// }
+
+
+
+
+
+ //TASK:13
+
+
+// var inp = prompt('enter your age')
+// if (inp<= 17){
+//         document.write('you are too young')
+//     }
+    
+//         else if(inp>19)
+//             {
+        
+//      document.write('you are too old')
+//             }
+
+
+
+
+
+//Task no:14
+
+// var inp=+prompt('enter steric"s length')
+// for (var i=0; i<=inp;i++){
+//   document.write('*')
+//   document.write('<br>')
+//   document.write('*')
+// }
+ 
+//     +----------+---------+----------+---------+------------+-----------+--------+-------+     //
+//                                      ~-+THE_END+-~                                            //
+
+
+
+
+
+
+
+
+//  var arry =  ['audi','lamborghini','volvo','mitshubishi']
+// //  console.log(arry.indexOf('audi'))
+// //  console.log(arry.lastIndexOf('mitshubishi'))
+// //  console.log(arry.pop())
+// // console.log(arry)
+ 
+
+
+
+
+
+ 
+
+
+
+ 
